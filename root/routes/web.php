4fcd4c3f9405b53ae1b0c7bcb0aed4b4cdc0447e
misaki.y <misaki.yokota@epkotsoftware.co.jp>
2024-07-11 11:28:42 +0900

    初回コミット

--- root/routes/web.php
+++ root/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::prefix('admin')->name('admin')->group(function(){
+    //admin admin
+    Route::view('','admin.index')->name('.index');//admin admin.index
+    // \App\Http\Controllers\JobController
+    Route::prefix('jobs')->name('.jobs')->controller(JobController::class)->group(function () {
+        // admin/jobs admin.jobs
+        Route::get('','index')->name('.index');// admin/jobs  admin.jobs.index > JobController@index
+        Route::post('','store')->name('.store');// admin/jobs  admin.jobs.store > JobController@store
+        Route::get('create','create')->name('.create');// admin/jobs/create  admin.jobs.create > JobController@create
+        Route::get('{job}','show')->name('.show');// admin/jobs/{job}  admin.jobs.show > JobController@show
+        Route::patch('{job}','update')->name('.update');// admin/jobs/{job}  admin.jobs.update > JobController@update
+        Route::delete('{job}','destroy')->name('.destroy');// admin/jobs/{job}  admin.jobs.destroy > JobController@destroy
+        Route::get('{job}/edit','edit')->name('.edit');// admin/jobs/{job}/edit  admin.jobs.edit > JobController@edit
+        Route::post('{job}/confirm','confirm')->name('confirm');// admin/jobs/{job}/confirm  admin.job.confirm > JobController@confirm
+    });
 });
